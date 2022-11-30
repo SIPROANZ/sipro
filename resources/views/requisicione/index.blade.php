da770698d29d7c49e10dc9b2a6443d1cd379994c
Amado <caicaguarec@gmail.com>
2022-11-30 02:48:09 -0400

    probando branch en requisicion

--- resources/views/requisicione/index.blade.php
+++ resources/views/requisicione/index.blade.php
@@ -65,6 +65,9 @@
 
                                             <td>
                                                 <form action="{{ route('requisiciones.destroy',$requisicione->id) }}" method="POST">
+                                                    <!-- Agregar detalles BOS a la requisicion -->
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('requisiciones.show',$requisicione->id) }}"><i class="fa fa-fw fa-eye"></i> Detalles BOS</a>
+                                                    
                                                     <a class="btn btn-sm btn-primary " href="{{ route('requisiciones.show',$requisicione->id) }}"><i class="fa fa-fw fa-eye"></i> Ver</a>
                                                     <a class="btn btn-sm btn-success" href="{{ route('requisiciones.edit',$requisicione->id) }}"><i class="fa fa-fw fa-edit"></i> Editar</a>
                                                     @csrf
